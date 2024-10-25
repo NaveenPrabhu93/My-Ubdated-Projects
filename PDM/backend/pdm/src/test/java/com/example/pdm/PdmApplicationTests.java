f8978a236e419d86bfe7db25ec5bba20252b80de
NaveenPrabhu93 <165675309+NaveenPrabhu93@users.noreply.github.com>
2024-10-25 16:36:05 +0530

    Add files via upload

---
+++ PDM/backend/pdm/src/test/java/com/example/pdm/PdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
